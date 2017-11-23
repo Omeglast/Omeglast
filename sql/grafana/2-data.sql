a55f03c72401ede147599e2fdec1bf62ec49fde7
Julien MERCIER <devci@j3ck3l.me>
2017-11-23 09:03:39 +0100

    Rename services and paths

--- sql/grafana/2-data.sql
+++ sql/grafana/2-data.sql
@@ -1,5 +1,5 @@
 \connect grafana;
 
 INSERT INTO "data_source" ("id", "org_id", "version", "type", "name", "access", "url", "password", "user", "database", "basic_auth", "basic_auth_user", "basic_auth_password", "is_default", "json_data", "created", "updated", "with_credentials", "secure_json_data") VALUES
-  (1,	1,	1,	'postgres',	'omeglast',	'proxy',	'storage',	'admin',	'admin',	'omeglast',	'0',	'',	'',	'1',	'{"sslmode":"disable"}',	'2017-11-13 19:09:27',	'2017-11-13 19:09:30',	'0',	'{"password":"eEczQ0xIbleNgCE2g/CkMOVLb/Gh8is7Bh51/q4="}');
+  (1,	1,	1,	'postgres',	'omeglast',	'proxy',	'mqtt-storage',	'admin',	'admin',	'omeglast',	'0',	'',	'',	'1',	'{"sslmode":"disable"}',	'2017-11-13 19:09:27',	'2017-11-13 19:09:30',	'0',	'{"password":"eEczQ0xIbleNgCE2g/CkMOVLb/Gh8is7Bh51/q4="}');
 
